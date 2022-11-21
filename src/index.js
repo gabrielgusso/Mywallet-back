0e23a31bfe32ad089b13c52277bd652a089afa60
gabrielgusso <gabriel.gusso@hotmail.com>
2022-11-21 14:24:49 -0300

    Update port for deploy

--- src/index.js
+++ src/index.js
@@ -11,5 +11,6 @@ app.use(express.json())
 app.use(activitesRouters)
 app.use(authRouters)
 
+const port = process.env.PORT || 5000
 
-app.listen(5000, () => console.log("Server running in port: 5000"))
+app.listen(port, () => console.log(`Server running in port: ${port}`))
